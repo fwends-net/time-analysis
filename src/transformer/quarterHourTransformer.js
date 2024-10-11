3b085c4f5fe9a5fd0ef1bef5d079fd01dd0eb47f
Peter Riegersperger <peter@fwends.net>
2024-10-11 12:50:58 +0200

    Update quarterHourTransformer.js

--- src/transformer/quarterHourTransformer.js
+++ src/transformer/quarterHourTransformer.js
@@ -1,24 +1,54 @@
 import dayjs from "dayjs";
-import {COL_START,COL_END}  from '../common/constants.js';
+import { COL_START, COL_END } from "../common/constants.js";
 /**
  * Splits time entries from input into 15 minute intervals.
  * These intervals start at 0, 15, 30 or 45 minutes after the hour.
- * 
+ *
  * If an entry fills out more than half of this specific interval, the complete
  * interval gets assigned to the entry.
- * 
+ *
  * For example, the entry 14.07 - 14.30 will get the intervals 14.00, 14.15
  * But the entry 14.08 - 14.30 will only the the interval 14.15, with the
  * interval 14.00 granted to its predecessor.
- * 
- * @param {*} input 
- * @returns 
+ *
+ * @param {*} input
+ * @returns
  */
 export function transform(input) {
+  // get correct time boundaries.
+  let temp = roundTimeBoundaries(input);
+
   let out = [];
 
+  for (let i = 0; i < temp.length; i++) {
+    // starting at the original start time, we continue to add
+    // 15 minutes as long as we are still before the original end time
+    // this divides up the whole time entry into 15 minute slots.
+    // these slots get new start and end times, but all other information
+    // is retained from the original entry.
+    let x = dayjs(temp[i][COL_START]);
+    while ( x.isBefore(dayjs(temp[i][COL_END]))) {
+      let newEntry = [...temp[i]];
+      newEntry[COL_START] = x.format('YYYY-MM-DD HH:mm');
+      x = x.add(15, 'minute');
+      newEntry[COL_END] = x.format('YYYY-MM-DD HH:mm');
+      out.push(newEntry);
+    }
+  }
 
-  for (let i=0; i < input.length; i++) {
+  return out;
+}
+
+/**
+ * Rounds the time boundaries of all entries to the nearest multiple of 15 minutes.
+ *
+ * @param {*} input
+ * @returns
+ */
+function roundTimeBoundaries(input) {
+  let out = [];
+
+  for (let i = 0; i < input.length; i++) {
     const newStart = round(input[i][COL_START]);
     const newEnd = round(input[i][COL_END]);
     if (newStart !== newEnd) {
@@ -28,9 +58,8 @@ export function transform(input) {
       //out.splice(i,1);
       let temp = [...input[i]];
       temp[COL_START] = newStart;
-      temp[COL_END] = newEnd;  
+      temp[COL_END] = newEnd;
       out.push(temp);
-   
     }
   }
 
@@ -39,30 +68,25 @@ export function transform(input) {
 
 /**
  * Rounds the date to the nearest quarter hour (0, 15, 30, 45)
- * @param {*} date 
- * @returns 
+ * @param {*} date
+ * @returns
  */
 function round(date) {
-  const minute = dayjs(date).get('minute');
+  const minute = dayjs(date).get("minute");
   let val = 0;
   if (minute < 8) {
     val = 0;
-  }
- else if (minute < 23) {
+  } else if (minute < 23) {
     val = 15;
-  }
-  else if (minute < 38) {
+  } else if (minute < 38) {
     val = 30;
-  }
-  else if (minute < 53) {
+  } else if (minute < 53) {
     val = 45;
-  }
-  else if (minute > 52) {
+  } else if (minute > 52) {
     val = 60;
   }
-  console.log('Round result: %s becomes %s', minute, val);
-  return dayjs(date).set('minute', val).format('YYYY-MM-DD HH:mm');
-
+  console.log("Round result: %s becomes %s", minute, val);
+  return dayjs(date).set("minute", val).format("YYYY-MM-DD HH:mm");
 }
 /*
 this is likely not needed at all.
