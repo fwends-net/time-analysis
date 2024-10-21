1578707e0422153dcf29460648e6a73534eca246
Peter Riegersperger <peter@fwends.net>
2024-10-21 10:24:34 +0200

    checks for correct dates and correct length

--- src/verify/lengthVerifier.js
+++ src/verify/lengthVerifier.js
@@ -1,6 +1,7 @@
 
 export function isValid(input) {
   for (let i=0; i < input.length; i++){
+    console.debug(input[i].length);
     if (input[i].length < 4) {
       console.error('Error in input. Entries must have at least four data fields:');
       console.error(input[i]);
